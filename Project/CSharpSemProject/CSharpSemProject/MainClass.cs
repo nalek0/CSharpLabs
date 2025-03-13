5840f0914f4972266b433e4bd72a5d59c2f501db
nalek0 <belash.aleksandr777@gmail.com>
2025-03-13 11:25:31 +0300

    Project simple unit testing

---
+++ Project/CSharpSemProject/CSharpSemProject/MainClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSemProject
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            ReportData reportData = null;
+            Console.WriteLine(reportData);
+        }
+    }
+}
---
+++ Project/CSharpSemProject/SemProjectUnitTesting/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace SemProjectUnitTesting
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
