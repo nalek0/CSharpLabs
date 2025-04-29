56cf5233393e90c3e0f399d8d0f319209576817c
nalek0 <belash.aleksandr777@gmail.com>
2025-04-29 19:23:28 +0300

    Initial Lab5 Task2 commit

---
+++ Lab5/Task2/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Data Source=localhost\\SQLEXPRESS;" +
+                "Initial Catalog=Lab5Task2Database;" +
+                "Integrated Security=True;";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                var query = "SELECT * FROM Players";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"PlayerID: {reader["PlayerID"]}");
+                    }
+                }
+            }
+        }
+    }
+}
